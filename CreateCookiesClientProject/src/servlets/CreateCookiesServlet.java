e591de846628e9cb7f41663744b1d3697dbfb2a1
Azra Balic <Azra Balic@192.168.2.166>
2016-02-08 13:03:20 +0100

    Signed-off-by: Azra Balic <Azra Balic@192.168.2.166>

--- CreateCookiesClientProject/src/servlets/CreateCookiesServlet.java
+++ CreateCookiesClientProject/src/servlets/CreateCookiesServlet.java
@@ -1,11 +1,18 @@
 package servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import entity.ejb.Customer;
+import facade.FacadeLocal;
+
 
 /**
  * Servlet implementation class CreateCookiesServlet
@@ -13,20 +20,36 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/CreateCookiesServlet")
 public class CreateCookiesServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public CreateCookiesServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	
+	@EJB
+	FacadeLocal facade;
 
 	/**
-	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public CreateCookiesServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void service(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		
+		out.println("</head><body>");
+		out.println("<h3>Customer</h3>" + "<br>");
+		out.println("** Create Customer **" + "<br>");
+		Customer c1 = new Customer();
+		c1.setcNumber(1);
+		c1.setcName("Mormors bageri");
+		c1.setcAddress("Lund");
+		facade.createCustomer(c1);
+		out.println("</body></html>");
 	}
 
 }
