b55cfd0d1cf26596097aa35c830336d096fb8399
Erik Åberg <erik@gustafsson-aberg.se>
2016-02-09 17:00:28 +0100

    Servlet

--- CreateCookiesClientProject/src/servlets/CreateCookiesServlet.java
+++ CreateCookiesClientProject/src/servlets/CreateCookiesServlet.java
@@ -13,14 +13,13 @@ import javax.servlet.http.HttpServletResponse;
 import entity.ejb.Customer;
 import facade.FacadeLocal;
 
-
 /**
  * Servlet implementation class CreateCookiesServlet
  */
 @WebServlet("/CreateCookiesServlet")
 public class CreateCookiesServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
+
 	@EJB
 	FacadeLocal facade;
 
@@ -36,8 +35,8 @@ public class CreateCookiesServlet extends HttpServlet {
 	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
-	protected void service(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		PrintWriter out = response.getWriter();
 		out.println("<!DOCTYPE html><html><head>");
@@ -46,11 +45,11 @@ public class CreateCookiesServlet extends HttpServlet {
 		out.println("</head><body>");
 		out.println("<h3>Customer</h3>" + "<br>");
 		out.println("** Create Customer **" + "<br>");
-//		Customer c1 = new Customer();
-//		c1.setcNumber(1);
-//		c1.setcName("Mormors bageri");
-//		c1.setcAddress("Lund");
-//		facade.createCustomer(c1);
+		Customer c1 = new Customer();
+		c1.setcNumber("1");
+		c1.setcName("Mormors bageri");
+		c1.setcAddress("Lund");
+		facade.createCustomer(c1);
 		out.println("</body></html>");
 	}
 
