e320a5d72eb1ef4c66804ea73cfdb9706e3df806
Azra Balic <midnattsviolen@gmail.com>
2016-02-12 12:33:12 +0100

    Signed-off-by: Azra Balic <midnattsviolen@gmail.com>

--- CreateCookiesProject/ejbModule/entity/ejb/Orderspecification.java
+++ CreateCookiesProject/ejbModule/entity/ejb/Orderspecification.java
@@ -14,6 +14,7 @@ import javax.persistence.Table;
 public class Orderspecification {
 	
 	private OrderspecificationId pNumberONumber;
+	private int palletInStock;
 	private Order order;
 	private Product product;
 
@@ -24,6 +25,15 @@ public class Orderspecification {
 
 	public void setpNumberONumber(OrderspecificationId pNumberONumber) {
 		this.pNumberONumber = pNumberONumber;
+	}
+	
+	@Column(name="palletInStock")
+	public int getPalletInStock() {
+		return palletInStock;
+	}
+
+	public void setPalletInStock(int palletInStock) {
+		this.palletInStock = palletInStock;
 	}
 
 	@ManyToOne
