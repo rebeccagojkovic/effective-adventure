6f8c0899520235f47698d49004286b09c17f39bb
Erik Åberg <erik@gustafsson-aberg.se>
2016-03-12 17:06:24 +0100

    Cleanup lsearch

--- CreateCookiesWeb3/js/lsearch.js
+++ CreateCookiesWeb3/js/lsearch.js
@@ -1,4 +1,5 @@
 $('#search').keyup(function () {
+    "use strict";
     var searchField = $('#search').val();
     var myExp = new RegExp(searchField, 'i');
     $.getJSON('http://iis.infoteket.nu:8080/CreateCookiesClientProject/ProductTable', function (data) {
@@ -7,14 +8,14 @@ $('#search').keyup(function () {
             $.each(obj, function (key, val) {
                 if ((val.pName.search(myExp) !== -1)) {
 
-                    output += '<div class="productRow">'
-                    output += '<article class="productInfo">'
-                    output += '<div><img alt="sample" src="eCommerceAssets/images/' + val.pNumber + '.jpg"></div>'
-                    output += '<p class="price">20kr</p>'
+                    output += '<div class="productRow">';
+                    output += '<article class="productInfo">';
+                    output += '<div><img alt="sample" src="eCommerceAssets/images/' + val.pNumber + '.jpg"></div>';
+                    output += '<p class="price">20kr</p>';
                     output += '<p class="productContent">' + val.pName + '</p>';
-                    output += '<input type="button" name="button" value="Köp" class="buyButton">'
-                    output += '</article>'
-                    output += '</div>'
+                    output += '<input type="button" name="button" value="Köp" class="buyButton">';
+                    output += '</article>';
+                    output += '</div>';
 
 
                 }
