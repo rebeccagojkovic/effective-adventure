4f0ea06964e46c9a8d38a1f077156f91d89a8540
Erik Åberg <erik@gustafsson-aberg.se>
2016-03-12 13:29:19 +0100

    Beutify ;)

--- CreateCookiesWeb3/js/lsearch.js
+++ CreateCookiesWeb3/js/lsearch.js
@@ -1,27 +1,27 @@
 $('#search').keyup(function () {
     var searchField = $('#search').val();
     var myExp = new RegExp(searchField, 'i');
-    $.getJSON('json/data.json', function (data) {
+    $.getJSON('http://iis.infoteket.nu:8080/CreateCookiesClientProject/ProductTable', function (data) {
         var output = '<ul class="searchresult">';
-        $.each(data, function(idx, obj){ 
-        $.each(obj, function(key, val){
-            if ((val.pName.search(myExp) !== -1)) {
-			
-				output += '<div class="productRow">'
-				output += '<article class="productInfo">'
-				output += '<div><img alt="sample" src="eCommerceAssets/images/berliners.jpg"></div>'
-				output += '<p class="price">20kr</p>'
-                output += '<p class="productContent">' + val.pName + '</p>';
-				output += '<input type="button" name="button" value="Köp" class="buyButton">'
-				output += '</article>'
-				output += '</div>'      
-		
-			  
+        $.each(data, function (idx, obj) {
+            $.each(obj, function (key, val) {
+                if ((val.pName.search(myExp) !== -1)) {
+
+                    output += '<div class="productRow">'
+                    output += '<article class="productInfo">'
+                    output += '<div><img alt="sample" src="eCommerceAssets/images/berliners.jpg"></div>'
+                    output += '<p class="price">20kr</p>'
+                    output += '<p class="productContent">' + val.pName + '</p>';
+                    output += '<input type="button" name="button" value="Köp" class="buyButton">'
+                    output += '</article>'
+                    output += '</div>'
+
+
                 }
+            });
+
         });
-		
-		});
-        
+
         $('#mainContent').html(output);
     });
 });
