5712e9f0d99cb1f43ce21ff2190114eccf1c0b29
Erik Åberg <erik@gustafsson-aberg.se>
2016-03-16 21:33:56 +0100

    Search i popup

--- CreateCookiesWeb3/js/submit.js
+++ CreateCookiesWeb3/js/submit.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-    $('#post').submit(function() {
-        window.open('', 'formpopup', 'width=400,height=400,resizeable');
+    $('.post').submit(function() {
+        window.open('', 'formpopup', 'width=400,height=400,top=100, left=100');
         this.target = 'formpopup';
 
     });
