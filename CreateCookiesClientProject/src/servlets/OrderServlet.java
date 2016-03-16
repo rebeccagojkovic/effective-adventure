33ea9d46db5b641a6c400518c259727594b9db42
rickardcederlund <rickard.cederlund@hotmail.com>
2016-03-16 17:04:23 +0100

    OrderServlet

---
+++ CreateCookiesClientProject/src/servlets/OrderServlet.java
@@ -0,0 +1,85 @@
+package servlets;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import entity.ejb.Order;
+import facade.FacadeLocal;
+
+/**
+ * Servlet implementation class OrderServlet
+ */
+@WebServlet("/OrderServlet")
+public class OrderServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	@EJB
+	FacadeLocal facade;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public OrderServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		throws ServletException, IOException {
+			// doGet(request, response);
+			String url = null;
+			// Get hidden field
+			String operation = request.getParameter("operation");
+			if (operation.equals("showorder")) {
+				System.out.println("MainServlet-showorder");
+
+				String oNumber = request.getParameter("orderNumber");
+				String isDelivered = request.getParameter("isDelivered");
+				
+
+				String searchOrder = request.getParameter("searchOrder");
+				
+				if (request.getParameter("radioOrderSearch") != null) {
+					if (request.getParameter("radioOrderSearch").equals("orderNumber")) {
+						request.setAttribute("searchOrder", orderNumber);
+
+
+						System.out.println("findByoNumber");
+
+						List<Order> orderNumber = facade.findByOrdernumber(searchOrder);
+						for (Order o1 : orderNumber) {
+							request.setAttribute("Order", o1);
+
+						}
+						url = "/ShowOrder.jsp";
+					
+					
+					}
+					if (request.getParameter("radioOrderSearch").equals("isDelivered")) {
+						request.setAttribute("searchOrder", isDelivered);
+
+						List<Order> isDelivered = facade.isDelivered(searchOrder);
+						for (Order o3 : isDelivered) {
+							request.setAttribute("Order", o3);
+						}
+						url = "/ShowOrder.jsp";
+					}
+
+				} else if (operation.equals("SearchOrder")) {
+					System.out.println("OrderServlet-SearchOrder");
+					url = "/SearchOrder.jsp";
+				} else {
+					url = "/SearchOrder.jsp";
+				}
+				System.out.println(url);
+				RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(url);
+				dispatcher.forward(request, response);
+			}
+		}
+}
