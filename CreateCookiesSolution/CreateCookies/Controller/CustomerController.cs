e228c6aead495c6931e6d47ac5479b7440b4b7d6
rickardcederlund <rickard.cederlund@hotmail.com>
2016-04-08 15:59:47 +0200

    CustomerController starting up dawg

--- CreateCookiesSolution/CreateCookies/Controller/CustomerController.cs
+++ CreateCookiesSolution/CreateCookies/Controller/CustomerController.cs
@@ -4,9 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace CreateCookies.Controller
 {
     class CustomerController
     {
+        private Form1 gui;
+        private CreateCookiesDataSet db;
+
+
+        public CustomerController(Form1 gui, CreateCookiesDataSet db)
+        {
+
+            this.db = db;
+            this.gui = gui;
+    
+            
+
+        }
+       
     }
 }
