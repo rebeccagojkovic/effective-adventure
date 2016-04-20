5d0f28c1a09b1191a53106640874d2f6f1d5d851
rickardcederlund <rickard.cederlund@hotmail.com>
2016-04-20 15:19:29 +0200

    Created constructors in Controller!!!!!!!!!!!!!!

--- CreateCookiesSolution/CreateCookies/Controller/Controller.cs
+++ CreateCookiesSolution/CreateCookies/Controller/Controller.cs
@@ -22,5 +22,40 @@ namespace CreateCookies.Controller
         {
             CDAL.RegisterCustomerQuery(cNumber, cAddress, cCountry, cEmail, cName, cPostalAddress);
         }
+        public void Ingredient(string iNumber, string iName, double iQuantityInStock, List<Recipe> recipe, Supplier supplier)
+        {
+            
+        }
+        public void Order(string oNumber, bool isDelivered, DateTime expectedDeliveryDate, Customer customer, List<orderspecification> orderspecification, List<Pallet> pallet, string oNumber)
+        {
+            
+        }
+
+        public void Orderspecification(int palletQuantity, Order order, Product product, int PalletQuantity)
+        {
+
+        }
+        public void Pallet(string palletNumber, DateTime palletTime, string pNumber, string oNumber, Order order, Product product, string PalletNumber)
+        {
+
+        }
+        public void Product(string pNumber, string pName, DateTime pTime, double price, List<Recipe> recipe, List<orderspecification> orderspecification, List<Pallet> pallet)
+        {
+
+
+        }
+        public void Recipe(double quantity, Ingredient ingredient, Product product)
+        {
+
+        }
+
+        public void Supplier(string sNumber, string sLocation, List<Ingredient> ingredient)
+        {
+
+        }
+        private string sNumber;
+        private string sLocation;
+        private List<Ingredient> ingredient;
+
     }
 }
