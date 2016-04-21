9abd2b8d2fd022d9cceadf58c52697454fe7849e
Erik Åberg <erik@gustafsson-aberg.se>
2016-04-21 14:45:03 +0200

    Dela upp Controller

--- CreateCookiesSolution/CreateCookies/Controller/Controller.cs
+++ CreateCookiesSolution/CreateCookies/Controller/Controller.cs
@@ -12,52 +12,10 @@ namespace CreateCookies.Controller
 {
     public class Controller
     {
-        //CreateCookiesDataSetTableAdapters.CustomerTableAdapter CDAL = new CreateCookiesDataSetTableAdapters.CustomerTableAdapter();
 
         public Controller()
         {
 
         }
-        //public void RegisterCustomer(string cNumber, string cName, string cAddress)
-        //{
-        //    CDAL.RegisterCustomerQuery(cNumber, cName, cAddress);
-        //}
-        public void Customer(string cNumber, string cName, string cAddress)
-        {
-
-        }
-        public void Ingredient(string iNumber, string iName, double iQuantityInStock)
-        {
-            
-        }
-        public void Order(string oNumber, bool isDelivered, DateTime expectedDeliveryDate)
-        {
-            
-        }
-
-        public void Orderspecification(int palletQuantity)
-        {
-
-        }
-        public void Pallet(string palletNumber, DateTime palletTime, string pNumber, string oNumber)
-        {
-
-        }
-        public void Product(string pNumber, string pName, DateTime pTime, double price)
-        {
-
-
-        }
-        public void Recipe(double quantity)
-        {
-
-        }
-
-        public void Supplier(string sNumber, string sLocation)
-        {
-
-        }
-        
-
     }
 }
---
+++ CreateCookiesSolution/CreateCookies/Controller/CustomerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateCookies.Controller
+{
+    class CustomerController
+    {
+        //CreateCookiesDataSetTableAdapters.CustomerTableAdapter CDAL = new CreateCookiesDataSetTableAdapters.CustomerTableAdapter();
+
+        //public void RegisterCustomer(string cNumber, string cName, string cAddress)
+        //{
+        //    CDAL.RegisterCustomerQuery(cNumber, cName, cAddress);
+        //}
+        public void Customer(string cNumber, string cName, string cAddress, string cPostalAddress, string cCountry, string cEmail)
+        {
+
+        }
+    }
+}
