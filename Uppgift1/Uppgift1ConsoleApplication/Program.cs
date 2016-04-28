dcb6415cd65eac2238f28a109ddda46ff9038aaf
Erik Åberg <erik@gustafsson-aberg.se>
2016-04-28 16:45:28 +0200

    Uppgift1

---
+++ Uppgift1/Uppgift1ConsoleApplication/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift1ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            localhost.WebService obj = new localhost.WebService();
+            Console.WriteLine("Startar");
+            Console.Write("Skriv in filnamn (test.txt) \n");
+            String filename = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("\n" + obj.txtFile(filename));
+            Console.WriteLine("\n ------------");
+            Console.Read();
+        }
+    }
+}
