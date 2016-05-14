47f381c8fa390e67077312fa038fd7488afe3376
Erik Åberg <erik@gustafsson-aberg.se>
2016-05-14 22:20:03 +0200

    Lite java igen

---
+++ NavJava/src/navjava/Employee.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package navjava;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author erik.aberg
+ */
+public class Employee {
+
+    public Employee() {
+
+        ArrayList<String> empl = new ArrayList<String>();
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+        
+        
+
+        // ArrayList<Controller.getEmployees()> empl = new ArrayList<Controller.getEmployees()>();
+    }
+}
