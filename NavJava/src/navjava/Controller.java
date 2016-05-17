c6eda1ace046d2a42684f26a89eab428c8e6171a
rickardcederlund <rickard.cederlund@hotmail.com>
2016-05-17 19:21:41 +0200

    navJava

---
+++ NavJava/src/navjava/Controller.java
@@ -0,0 +1,154 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package navjava;
+
+import javax.swing.table.DefaultTableModel;
+import org.tempuri.ArrayOfEmployee;
+import org.tempuri.ArrayOfEmployeeAbsence;
+import org.tempuri.ArrayOfEmployeeMeta;
+import org.tempuri.ArrayOfEmployeeRelative;
+
+/**
+ *
+ * @author erik.aberg
+ */
+public class Controller {
+
+    public static void addEmployee(java.lang.String id, java.lang.String firstName, java.lang.String middleName, java.lang.String lastName, java.lang.String initials, java.lang.String jobTitle, java.lang.String searchName, java.lang.String adress, java.lang.String adress2, java.lang.String city, java.lang.String postCode, java.lang.String county, java.lang.String phoneNumber, java.lang.String mobilePhoneNumber, java.lang.String eMail, java.lang.String altAdress, javax.xml.datatype.XMLGregorianCalendar altAdressStart, javax.xml.datatype.XMLGregorianCalendar altAdressEnd, java.lang.String picture, javax.xml.datatype.XMLGregorianCalendar birthDate, java.lang.String socialSecurityNumber, java.lang.String unionCode, java.lang.String unionMembershipNumber, int sex, java.lang.String countryRegionCode, java.lang.String managerNumber, java.lang.String employmentContractCode, java.lang.String statisticsGroupCode, javax.xml.datatype.XMLGregorianCalendar employmentDate, int status, javax.xml.datatype.XMLGregorianCalendar inactivityDate, java.lang.String causeOfInactivity, javax.xml.datatype.XMLGregorianCalendar terminationDate, java.lang.String groundsForTermCode, java.lang.String globalDimension1Code, java.lang.String globalDimension2Code, java.lang.String resourceNumber, javax.xml.datatype.XMLGregorianCalendar lastDateModified, java.lang.String extension, java.lang.String pager, java.lang.String faxNumber, java.lang.String companyEmail, java.lang.String title, java.lang.String salesPerPurchCode, java.lang.String noSeries) {
+        org.tempuri.WebService1 service = new org.tempuri.WebService1();
+        org.tempuri.WebService1Soap port = service.getWebService1Soap12();
+        port.addEmployee(id, firstName, middleName, lastName, initials, jobTitle, searchName, adress, adress2, city, postCode, county, phoneNumber, mobilePhoneNumber, eMail, altAdress, altAdressStart, altAdressEnd, picture, birthDate, socialSecurityNumber, unionCode, unionMembershipNumber, sex, countryRegionCode, managerNumber, employmentContractCode, statisticsGroupCode, employmentDate, status, inactivityDate, causeOfInactivity, terminationDate, groundsForTermCode, globalDimension1Code, globalDimension2Code, resourceNumber, lastDateModified, extension, pager, faxNumber, companyEmail, title, salesPerPurchCode, noSeries);
+    }
+
+    public static void addEmployeeMini(java.lang.String id, java.lang.String firstName, java.lang.String lastName){
+         org.tempuri.WebService1 service = new org.tempuri.WebService1();
+        org.tempuri.WebService1Soap port = service.getWebService1Soap12();
+        port.addEmployeeMini(id, firstName, lastName);
+        
+    }
+    public static void deleteEmployee(java.lang.String id) {
+        org.tempuri.WebService1 service = new org.tempuri.WebService1();
+        org.tempuri.WebService1Soap port = service.getWebService1Soap12();
+        port.deleteEmployee(id);
+    }
+
+    public static ArrayOfEmployeeMeta getAllTables() {
+        org.tempuri.WebService1 service = new org.tempuri.WebService1();
+        org.tempuri.WebService1Soap port = service.getWebService1Soap12();
+        return port.getAllTables();
+    }
+
+    public static ArrayOfEmployeeMeta getAllTables2() {
+        org.tempuri.WebService1 service = new org.tempuri.WebService1();
+        org.tempuri.WebService1Soap port = service.getWebService1Soap12();
+        return port.getAllTables2();
+    }
+
+    public static ArrayOfEmployeeMeta getConstraints() {
+        org.tempuri.WebService1 service = new org.tempuri.WebService1();
+        org.tempuri.WebService1Soap port = service.getWebService1Soap12();
+        return port.getConstraints();
+    }
+
+    public static ArrayOfEmployee getEmployee(java.lang.String id) {
+        org.tempuri.WebService1 service = new org.tempuri.WebService1();
+        org.tempuri.WebService1Soap port = service.getWebService1Soap12();
+        return port.getEmployee(id);
+    }
+
+    public static ArrayOfEmployeeAbsence getEmployeeAbsence() {
+        org.tempuri.WebService1 service = new org.tempuri.WebService1();
+        org.tempuri.WebService1Soap port = service.getWebService1Soap12();
+        return port.getEmployeeAbsence();
+    }
+
+    public static ArrayOfEmployee getEmployees() {
+        org.tempuri.WebService1 service = new org.tempuri.WebService1();
+        org.tempuri.WebService1Soap port = service.getWebService1Soap12();
+        return port.getEmployees();
+    }
+
+    public static ArrayOfEmployeeMeta getEmployeesMeta() {
+        org.tempuri.WebService1 service = new org.tempuri.WebService1();
+        org.tempuri.WebService1Soap port = service.getWebService1Soap12();
+        return port.getEmployeesMeta();
+    }
+
+    public static ArrayOfEmployeeMeta getEmployeesMeta2() {
+        org.tempuri.WebService1 service = new org.tempuri.WebService1();
+        org.tempuri.WebService1Soap port = service.getWebService1Soap12();
+        return port.getEmployeesMeta2();
+    }
+
+    public static ArrayOfEmployeeMeta getIndexes() {
+        org.tempuri.WebService1 service = new org.tempuri.WebService1();
+        org.tempuri.WebService1Soap port = service.getWebService1Soap12();
+        return port.getIndexes();
+    }
+
+    public static ArrayOfEmployeeMeta getKeys() {
+        org.tempuri.WebService1 service = new org.tempuri.WebService1();
+        org.tempuri.WebService1Soap port = service.getWebService1Soap12();
+        return port.getKeys();
+    }
+
+    public static ArrayOfEmployeeRelative getRelative() {
+        org.tempuri.WebService1 service = new org.tempuri.WebService1();
+        org.tempuri.WebService1Soap port = service.getWebService1Soap12();
+        return port.getRelative();
+    }
+
+    public static ArrayOfEmployee getSickestEmployee() {
+        org.tempuri.WebService1 service = new org.tempuri.WebService1();
+        org.tempuri.WebService1Soap port = service.getWebService1Soap12();
+        return port.getSickestEmployee();
+    }
+
+    public static void updateEmployee(java.lang.String id, java.lang.String firstName) {
+        org.tempuri.WebService1 service = new org.tempuri.WebService1();
+        org.tempuri.WebService1Soap port = service.getWebService1Soap12();
+        port.updateEmployee(id, firstName);
+    }
+    public static DefaultTableModel GetEmployeesTable() {
+        return EmployeeTable.GetEmployeesTable();
+    }
+ public static DefaultTableModel GetEmployeesMetaTable() {
+        return EmployeeTable.GetEmployeesMetaTable();
+    }
+   
+     public static DefaultTableModel GetEmployeesMetaTable2() {
+        return EmployeeTable.GetEmployeesMetaTable2();
+    }
+     
+      public static DefaultTableModel GetConstraintsTable() {
+        return EmployeeTable.GetConstraintsTable();
+    }
+     public static DefaultTableModel GetEmployeeAbsenceTable() {
+        return EmployeeTable.GetEmployeeAbsenceTable();
+    }
+       public static DefaultTableModel GetEmployeeMeta() {
+        return EmployeeTable.GetEmployeesMeta();
+    }
+         public static DefaultTableModel GetEmployeeMeta2() {
+        return EmployeeTable.GetEmployeesMeta2();
+    }
+         public static DefaultTableModel GetIndexes() {
+        return EmployeeTable.GetIndexes();
+    }
+          public static DefaultTableModel GetKeys() {
+        return EmployeeTable.GetKeys();
+    }
+          public static DefaultTableModel GetRelative() {
+        return EmployeeTable.GetRelative();
+    }
+           public static DefaultTableModel GetSickestEmployee() {
+        return EmployeeTable.GetSickestEmployee();
+    }
+          
+             public static DefaultTableModel GetEmployee(String id) {
+        return EmployeeTable.GetEmployee(id);
+    }
+}
---
+++ NavJava/src/navjava/EmployeeTable.java
@@ -0,0 +1,241 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package navjava;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author erik.aberg
+ */
+public class EmployeeTable {
+
+    public static DefaultTableModel GetEmployeesTable() {
+
+        String columnNames[] = {"EmployeeNo", "FirstName", "LastName"};
+        List<org.tempuri.Employee> list = Controller.getEmployees().getEmployee();
+
+        DefaultTableModel model = new DefaultTableModel();
+
+        model.setColumnIdentifiers(columnNames);
+
+        for (org.tempuri.Employee s : list) {
+            Object[] o = new Object[5];
+            o[0] = s.getEmployeeNo();
+            o[1] = s.getFirstName();
+            o[2] = s.getLastName();
+
+            model.addRow(o);
+        }
+        return model;
+    }
+
+    public static DefaultTableModel GetEmployeesMetaTable2() {
+        String columnNames[] = {"EmployeeNo", "FirstName", "LastName"};
+        List<org.tempuri.EmployeeMeta> list = Controller.getEmployeesMeta2().getEmployeeMeta();
+
+        DefaultTableModel model = new DefaultTableModel();
+
+        model.setColumnIdentifiers(columnNames);
+
+        for (org.tempuri.EmployeeMeta s : list) {
+            Object[] o = new Object[5];
+            o[0] = s.getColumnName();
+            o[1] = s.getConstraintName();
+            o[2] = s.getTableName();
+            o[3] = s.getObjectID();
+
+            model.addRow(o);
+        }
+        return model;
+    }
+
+    public static DefaultTableModel GetEmployeesMetaTable() {
+        String columnNames[] = {"ColumnName", "ConstraintName", "TableName", "ObjectID"};
+        List<org.tempuri.EmployeeMeta> list = Controller.getEmployeesMeta().getEmployeeMeta();
+
+        DefaultTableModel model = new DefaultTableModel();
+
+        model.setColumnIdentifiers(columnNames);
+
+        for (org.tempuri.EmployeeMeta s : list) {
+            Object[] o = new Object[5];
+            o[0] = s.getColumnName();
+            o[1] = s.getConstraintName();
+            o[2] = s.getTableName();
+            o[3] = s.getObjectID();
+
+            model.addRow(o);
+        }
+        return model;
+    }
+
+    public static DefaultTableModel GetConstraintsTable() {
+        String columnNames[] = {"ConstraintName"};
+        List<org.tempuri.EmployeeMeta> list = Controller.getConstraints().getEmployeeMeta();
+
+        DefaultTableModel model = new DefaultTableModel();
+
+        model.setColumnIdentifiers(columnNames);
+
+        for (org.tempuri.EmployeeMeta s : list) {
+            Object[] o = new Object[1];
+
+            o[1] = s.getConstraintName();
+
+            model.addRow(o);
+        }
+        return model;
+    }
+
+    public static DefaultTableModel GetEmployeeAbsenceTable() {
+        String columnNames[] = {"CauseOfAnsenceCode", "EmployeeNumber"};
+        List<org.tempuri.EmployeeAbsence> list = Controller.getEmployeeAbsence().getEmployeeAbsence();
+
+        DefaultTableModel model = new DefaultTableModel();
+
+        model.setColumnIdentifiers(columnNames);
+
+        for (org.tempuri.EmployeeAbsence s : list) {
+            Object[] o = new Object[2];
+            o[0] = s.getCauseOfAbsenceCode();
+            o[1] = s.getEmployeeNo();
+
+            model.addRow(o);
+        }
+        return model;
+    }
+
+    public static DefaultTableModel GetEmployeesMeta() {
+        String columnNames[] = {"TableName", "MetaData"};
+        List<org.tempuri.EmployeeMeta> list = Controller.getEmployeesMeta().getEmployeeMeta();
+
+        DefaultTableModel model = new DefaultTableModel();
+
+        model.setColumnIdentifiers(columnNames);
+
+        for (org.tempuri.EmployeeMeta s : list) {
+            Object[] o = new Object[2];
+            o[0] = s.getTableName();
+            o[1] = s.getColumnName();
+
+            model.addRow(o);
+        }
+        return model;
+    }
+
+    public static DefaultTableModel GetEmployeesMeta2() {
+        String columnNames[] = {"TableName", "MetaData"};
+        List<org.tempuri.EmployeeMeta> list = Controller.getEmployeesMeta2().getEmployeeMeta();
+
+        DefaultTableModel model = new DefaultTableModel();
+
+        model.setColumnIdentifiers(columnNames);
+
+        for (org.tempuri.EmployeeMeta s : list) {
+            Object[] o = new Object[2];
+            o[0] = s.getTableName();
+            o[1] = s.getColumnName();
+
+            model.addRow(o);
+        }
+        return model;
+    }
+
+    public static DefaultTableModel GetIndexes() {
+        String columnNames[] = {"TableName", "ObjectID"};
+        List<org.tempuri.EmployeeMeta> list = Controller.getIndexes().getEmployeeMeta();
+
+        DefaultTableModel model = new DefaultTableModel();
+
+        model.setColumnIdentifiers(columnNames);
+
+        for (org.tempuri.EmployeeMeta s : list) {
+            Object[] o = new Object[2];
+            o[0] = s.getTableName();
+            o[1] = s.getObjectID();
+
+            model.addRow(o);
+        }
+        return model;
+    }
+
+    public static DefaultTableModel GetKeys() {
+        String columnNames[] = {"TableName", "ObjectID"};
+        List<org.tempuri.EmployeeMeta> list = Controller.getKeys().getEmployeeMeta();
+
+        DefaultTableModel model = new DefaultTableModel();
+
+        model.setColumnIdentifiers(columnNames);
+
+        for (org.tempuri.EmployeeMeta s : list) {
+            Object[] o = new Object[2];
+            o[0] = s.getTableName();
+            o[1] = s.getObjectID();
+
+            model.addRow(o);
+        }
+        return model;
+    }
+
+    public static DefaultTableModel GetRelative() {
+        String columnNames[] = {"EmployeeNumber", "LineNumber}", "RelativeCode"};
+        List<org.tempuri.EmployeeRelative> list = Controller.getRelative().getEmployeeRelative();
+
+        DefaultTableModel model = new DefaultTableModel();
+
+        model.setColumnIdentifiers(columnNames);
+
+        for (org.tempuri.EmployeeRelative s : list) {
+            Object[] o = new Object[3];
+            o[0] = s.getEmployeeNo();
+            o[1] = s.getLineNo();
+            o[2] = s.getRelativeCode();
+            model.addRow(o);
+        }
+        return model;
+    }
+     public static DefaultTableModel GetSickestEmployee() {
+        String columnNames[] = {"First Name"};
+        List<org.tempuri.Employee> list = Controller.getSickestEmployee().getEmployee();
+
+        DefaultTableModel model = new DefaultTableModel();
+
+        model.setColumnIdentifiers(columnNames);
+
+        for (org.tempuri.Employee s : list) {
+            Object[] o = new Object[1];
+            o[0] = s.getFirstName();
+          
+            model.addRow(o);
+        }
+        return model;
+    }
+     
+       public static DefaultTableModel GetEmployee(String id) {
+
+        String columnNames[] = {"EmployeeNo", "FirstName", "LastName"};
+        List<org.tempuri.Employee> list = Controller.getEmployee(id).getEmployee();
+
+        DefaultTableModel model = new DefaultTableModel();
+
+        model.setColumnIdentifiers(columnNames);
+
+        for (org.tempuri.Employee s : list) {
+            Object[] o = new Object[5];
+            o[0] = s.getEmployeeNo();
+            o[1] = s.getFirstName();
+            o[2] = s.getLastName();
+
+            model.addRow(o);
+        }
+        return model;
+    }
+  
+    
+}
