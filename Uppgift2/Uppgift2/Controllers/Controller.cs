cf1d3fe1052d31e86c92abbc0aded167bde60593
Erik Åberg <erik@gustafsson-aberg.se>
2016-05-18 11:44:13 +0200

    Uppgift2 MVC

---
+++ Uppgift2/Uppgift2/Controllers/Controller.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Uppgift2.Controllers
+{
+    public class Controller
+    {
+        private DAL.DAL dal = new DAL.DAL();
+
+        public Controller()
+        {
+
+        }
+        public List<Models.Customer> GetCustomersList()
+        {
+            return dal.GetCustomerList();
+        }
+        public List<string> GetCustomers()
+        {
+            return dal.GetCustomers();
+        }
+    }
+}
--- Uppgift2/Uppgift2/Models/Customer.cs
+++ Uppgift2/Uppgift2/Models/Customer.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace Uppgift2
+namespace Uppgift2.Models
 {
     public class Customer
     {
